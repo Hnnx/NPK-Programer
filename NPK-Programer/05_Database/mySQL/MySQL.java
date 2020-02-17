0bd78977864e398b3c0d2b5a7dc46d345bf18412
HNNX <nejc.stibilj01@gmail.com>
2020-02-17 19:29:49 +0100

    mySQL database

--- NPK-Programer/05_Database/mySQL/MySQL.java
+++ NPK-Programer/05_Database/mySQL/MySQL.java
@@ -12,32 +12,46 @@ import java.sql.Statement;
 
 public class MySQL {
 
-	public static void main(String[] args)  {
-		
+	public static void main(String[] args) throws InterruptedException 	{
+
 		Connection povezava;
 		Statement stmt = null;
-		
+
 		try {
+
+			//LINK DO DRIVERJA
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			String url = "jdbc:mysql://192.168.2.2:3306/employees";
 			povezava = DriverManager.getConnection(url, "npk", "npkgeslo");
-			
+
 			stmt = povezava.createStatement();
-			
-			ResultSet rs = stmt.executeQuery("SELECT * FROM employees LIMIT 500");
-			
+
+			ResultSet rs =stmt.executeQuery(
+
+"SELECT employees.first_name, employees.last_name, salaries.salary, salaries.from_date, salaries.to_date " +
+"FROM employees " +
+"JOIN salaries ON employees.emp_no = salaries.emp_no " +
+"WHERE employees.emp_no = 10003;");
+
+
 			while(rs.next() ) {
-				System.out.println(rs.getInt(1)+ " "+ rs.getString(2) +" " + rs.getString(3)+" "+rs.getString(4) );
-	
+
+				System.out.println(rs.getString(1)+
+						" "+ rs.getString(2) +
+						" "+ rs.getString(3) +
+						" "+ rs.getString(4) +
+						" " + rs.getString(5));
+
+
 			} 
-			
+
 			povezava.close();  
-					
-			
+
+
 		} catch (Exception e) {
 			System.out.println("Prislo je do napake: " + e.toString());
 		}
-				
+
 
 	}
 
